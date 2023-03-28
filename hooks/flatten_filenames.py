64562e3a564dc596e979c14ef0a9e12130c56853
Sumanth Peddamatham <peddamat@gmail.com>
2023-03-28 13:18:15 -0700

    fml.

--- hooks/flatten_filenames.py
+++ hooks/flatten_filenames.py
@@ -1,5 +1,3 @@
-<<<<<<< temp
-<<<<<<< temp2
 # import os
 # import sys
 # from timeit import default_timer as timer
@@ -35,73 +33,3 @@ def on_files(files, config):
             f.url = f.dest_path.replace("%20", "-").lower()
 
     return files
-|||||||
-=======
-# import os
-# import sys
-# from timeit import default_timer as timer
-# from datetime import datetime, timedelta
-
-# from mkdocs import utils as mkdocs_utils
-# from mkdocs.config import config_options, Config
-# from mkdocs.plugins import BasePlugin
-# from mkdocs.structure.files import Files
-
-# import logging
-# import mkdocs.utils
-# log = logging.getLogger(f"mkdocs.plugins.{__name__}")
-# log.addFilter(mkdocs.utils.warning_filter)
-
-import logging, re
-import mkdocs.plugins
-
-log = logging.getLogger('mkdocs')
-
-def on_files(files, config):
-    for f in files:
-        if f.is_documentation_page() or f.is_media_file():
-            f.abs_dest_path = f.abs_dest_path.replace(" ", "-").lower()
-            f.abs_dest_path = f.abs_dest_path.replace("(", "").lower()
-            f.abs_dest_path = f.abs_dest_path.replace(")", "").lower()
-            f.dest_path = f.dest_path.replace(" ", "-").lower()
-            f.dest_path = f.dest_path.replace("(", "").lower()
-            f.dest_path = f.dest_path.replace(")", "").lower()
-            f.url = f.dest_path.replace("%20", "-").lower()
-
-    return files
->>>>>>> Copied over changes from testing environment.
-|||||||
-=======
-# import os
-# import sys
-# from timeit import default_timer as timer
-# from datetime import datetime, timedelta
-
-# from mkdocs import utils as mkdocs_utils
-# from mkdocs.config import config_options, Config
-# from mkdocs.plugins import BasePlugin
-# from mkdocs.structure.files import Files
-
-# import logging
-# import mkdocs.utils
-# log = logging.getLogger(f"mkdocs.plugins.{__name__}")
-# log.addFilter(mkdocs.utils.warning_filter)
-
-import logging, re
-import mkdocs.plugins
-
-log = logging.getLogger('mkdocs')
-
-def on_files(files, config):
-    for f in files:
-        if f.is_documentation_page() or f.is_media_file():
-            f.abs_dest_path = f.abs_dest_path.replace(" ", "-").lower()
-            f.abs_dest_path = f.abs_dest_path.replace("(", "").lower()
-            f.abs_dest_path = f.abs_dest_path.replace(")", "").lower()
-            f.dest_path = f.dest_path.replace(" ", "-").lower()
-            f.dest_path = f.dest_path.replace("(", "").lower()
-            f.dest_path = f.dest_path.replace(")", "").lower()
-            f.url = f.dest_path.replace("%20", "-").lower()
-
-    return files
->>>>>>> Copied over changes from testing environment.
